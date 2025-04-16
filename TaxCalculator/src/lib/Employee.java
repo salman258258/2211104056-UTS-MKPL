f37d83888e00dcd7188a61fc631bafac48f88cce
salman258258 <lucifersalman93@gmail.com>
2025-04-16 19:52:19 +0700

    BugFix-SpouseSetter
    Fixed bug in setSpouse() method: corrected assignment of spouseIdNumber

--- TaxCalculator/src/lib/Employee.java
+++ TaxCalculator/src/lib/Employee.java
@@ -1,11 +1,15 @@
 package lib;
 
 import java.time.LocalDate;
-import java.time.Month;
 import java.util.LinkedList;
 import java.util.List;
 
 public class Employee {
+
+	private static final int SALARY_GRADE_1 = 3000000;
+	private static final int SALARY_GRADE_2 = 5000000;
+	private static final int SALARY_GRADE_3 = 7000000;
+	private static final double FOREIGNER_MULTIPLIER = 1.5;
 
 	private String employeeId;
 	private String firstName;
@@ -31,7 +35,8 @@ public class Employee {
 	private List<String> childNames;
 	private List<String> childIdNumbers;
 	
-	public Employee(String employeeId, String firstName, String lastName, String idNumber, String address, int yearJoined, int monthJoined, int dayJoined, boolean isForeigner, boolean gender) {
+	public Employee(String employeeId, String firstName, String lastName, String idNumber, String address,
+			int yearJoined, int monthJoined, int dayJoined, boolean isForeigner, boolean gender) {
 		this.employeeId = employeeId;
 		this.firstName = firstName;
 		this.lastName = lastName;
@@ -42,67 +47,72 @@ public class Employee {
 		this.dayJoined = dayJoined;
 		this.isForeigner = isForeigner;
 		this.gender = gender;
-		
-		childNames = new LinkedList<String>();
-		childIdNumbers = new LinkedList<String>();
+
+		childNames = new LinkedList<>();
+		childIdNumbers = new LinkedList<>();
 	}
-	
+
 	/**
-	 * Fungsi untuk menentukan gaji bulanan pegawai berdasarkan grade kepegawaiannya (grade 1: 3.000.000 per bulan, grade 2: 5.000.000 per bulan, grade 3: 7.000.000 per bulan)
-	 * Jika pegawai adalah warga negara asing gaji bulanan diperbesar sebanyak 50%
+	 * Fungsi untuk menentukan gaji bulanan pegawai berdasarkan grade kepegawaiannya.
+	 * Jika pegawai adalah warga negara asing, gaji bulanan diperbesar sebanyak 50%.
 	 */
-	
-	public void setMonthlySalary(int grade) {	
-		if (grade == 1) {
-			monthlySalary = 3000000;
-			if (isForeigner) {
-				monthlySalary = (int) (3000000 * 1.5);
-			}
-		}else if (grade == 2) {
-			monthlySalary = 5000000;
-			if (isForeigner) {
-				monthlySalary = (int) (3000000 * 1.5);
-			}
-		}else if (grade == 3) {
-			monthlySalary = 7000000;
-			if (isForeigner) {
-				monthlySalary = (int) (3000000 * 1.5);
-			}
+	public void setMonthlySalary(int grade) {
+		switch (grade) {
+			case 1:
+				monthlySalary = SALARY_GRADE_1;
+				break;
+			case 2:
+				monthlySalary = SALARY_GRADE_2;
+				break;
+			case 3:
+				monthlySalary = SALARY_GRADE_3;
+				break;
+			default:
+				monthlySalary = 0;
+		}
+
+		if (isForeigner) {
+			monthlySalary *= FOREIGNER_MULTIPLIER;
 		}
 	}
-	
-	public void setAnnualDeductible(int deductible) {	
+
+	public void setAnnualDeductible(int deductible) {
 		this.annualDeductible = deductible;
 	}
-	
-	public void setAdditionalIncome(int income) {	
+
+	public void setAdditionalIncome(int income) {
 		this.otherMonthlyIncome = income;
 	}
-	
+
 	public void setSpouse(String spouseName, String spouseIdNumber) {
 		this.spouseName = spouseName;
-		this.spouseIdNumber = idNumber;
+		this.spouseIdNumber = spouseIdNumber;
 	}
-	
+
 	public void addChild(String childName, String childIdNumber) {
 		childNames.add(childName);
 		childIdNumbers.add(childIdNumber);
 	}
-	
+
 	public int getAnnualIncomeTax() {
-		
-		//Menghitung berapa lama pegawai bekerja dalam setahun ini, jika pegawai sudah bekerja dari tahun sebelumnya maka otomatis dianggap 12 bulan.
+		// Menghitung berapa lama pegawai bekerja dalam setahun
 		LocalDate date = LocalDate.now();
-		
+
 		if (date.getYear() == yearJoined) {
 			monthWorkingInYear = date.getMonthValue() - monthJoined;
-		}else {
+		} else {
 			monthWorkingInYear = 12;
 		}
-		
-		TaxData data = new TaxData(monthlySalary, otherMonthlyIncome, monthWorkingInYear,
-        annualDeductible, spouseIdNumber.equals(""), childIdNumbers.size());
 
-return TaxFunction.calculateTax(data);
+		TaxData data = new TaxData(
+			monthlySalary,
+			otherMonthlyIncome,
+			monthWorkingInYear,
+			annualDeductible,
+			spouseIdNumber == null || spouseIdNumber.isEmpty(),
+			childIdNumbers.size()
+		);
+
+		return TaxFunction.calculateTax(data);
 	}
 }
